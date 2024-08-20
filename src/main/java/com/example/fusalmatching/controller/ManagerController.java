d8fa98e73414190a7034ea108f5ce0c099177207
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-08-20 21:46:33 +0900

    repository변경

--- src/main/java/com/example/fusalmatching/controller/ManagerController.java
+++ src/main/java/com/example/fusalmatching/controller/ManagerController.java
@@ -3,12 +3,10 @@ package com.example.fusalmatching.controller;
 import com.example.fusalmatching.config.jwt.JwtToken;
 import com.example.fusalmatching.dto.request.LoginRequestDto;
 import com.example.fusalmatching.dto.request.ManagerSignRequestDto;
+import com.example.fusalmatching.dto.response.ManagerResponseDto;
 import com.example.fusalmatching.service.ManagerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/managers")
@@ -17,6 +15,11 @@ public class ManagerController {
 
     private final ManagerService managerService;
 
+
+    @GetMapping
+    public ManagerResponseDto getMyPage(@RequestParam String id) {
+        return managerService.getMyPage(id);
+    }
 
     @PostMapping("/new")
     public void createManager(@RequestBody ManagerSignRequestDto managerSignRequestDto) throws Exception {
